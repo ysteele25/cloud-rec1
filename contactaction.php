501dd3b1f53678233362e33716217440af67736c
rohit679 <rohitprasad25061999@gmail.com>
2022-09-01 12:02:00 +0530

    edfgr

--- contactaction.php
+++ contactaction.php
@@ -1,9 +1,10 @@
 <?php
- $to = "rohit.prasad@plaxonic.com";
-// $subject = "This is subject";
+echo 'heloooo';
+//  $to = "rohit.prasad@plaxonic.com";
+// // $subject = "This is subject";
 
-$message = "<b>This is HTML message.</b>";
-$message .= "<h1>This is headline.</h1>";
+// $message = "<b>This is HTML message.</b>";
+// $message .= "<h1>This is headline.</h1>";
 
 // $header = "From:abc@somedomain.com \r\n";
 // $header .= "Cc:afgh@somedomain.com \r\n";
@@ -18,29 +19,29 @@ $message .= "<h1>This is headline.</h1>";
 // echo "Message could not be sent...";
 // }
 
-require 'phpmailer/PHPMailerAutoload.php';
+// require 'phpmailer/PHPMailerAutoload.php';
 
-$mail = new PHPMailer;
-			$mail->isSMTP();
-			$mail->Host = 'smtp.gmail.com';
-			$mail->Port = 465;
-			$mail->SMTPSecure = 'ssl';
-			$mail->SMTPAuth = true;
-			$mail->Username = "testingtech57@gmail.com";
-			$mail->Password = "Incorrect@123";
-			$mail->FromName = "Test";
-			$mail->addAddress($to);
-			$mail->Subject = "Test SMTP";
-			$mail->msgHTML($message);
+// $mail = new PHPMailer;
+// 			$mail->isSMTP();
+// 			$mail->Host = 'smtp.gmail.com';
+// 			$mail->Port = 465;
+// 			$mail->SMTPSecure = 'ssl';
+// 			$mail->SMTPAuth = true;
+// 			$mail->Username = "testingtech57@gmail.com";
+// 			$mail->Password = "Incorrect@123";
+// 			$mail->FromName = "Test";
+// 			$mail->addAddress($to);
+// 			$mail->Subject = "Test SMTP";
+// 			$mail->msgHTML($message);
 
 
-      if ($mail->send())
-{
-  echo 'mail sent';
-}
-else{
-  echo 'not sent';
-}
+//       if ($mail->send())
+// {
+//   echo 'mail sent';
+// }
+// else{
+//   echo 'not sent';
+// }
 
 
 
